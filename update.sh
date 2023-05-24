5895ada066b2e243c5c8e13d74f8662ad5804bce
Terry Zhang <zterry@qq.com>
2023-05-24 10:08:53 +0800

    php 8.2 support

--- update.sh
+++ update.sh
@@ -9,7 +9,7 @@ sedStr="
 sed -r "$sedStr" $1
 }
 
-versions=(7.1 7.2 7.3 7.4 8.0 8.1)
+versions=(7.1 7.2 7.3 7.4 8.0 8.1 8.2)
 for version in ${versions[*]}; do
   if [ ! -d ${version}/alpine ]; then
     mkdir -p ${version}/alpine
@@ -37,7 +37,7 @@ for version in ${versions[*]}; do
   fi
   # https://www.php.net/manual/en/image.installation.php
   # php 7.4 gd config differenct as before.
-  if [[ "${version}" = "7.4" || "${version}" = "8.0" || "${version}" = "8.1" ]]; then
+  if [[ "${version}" = "7.4" || "${version}" = "8.0" || "${version}" = "8.1" || "${version}" = "8.2" ]]; then
     sed -i "s/with-gd/enable-gd/g" ${version}/alpine/Dockerfile
     sed -i "s/--with-png-dir=\/usr\/include\///g" ${version}/alpine/Dockerfile
     sed -i "s/-dir=/=/g" ${version}/alpine/Dockerfile
@@ -70,7 +70,7 @@ for version in ${versions[*]}; do
   fi
   # https://www.php.net/manual/en/image.installation.php
   # php 7.4 gd config differenct as before.
-  if [[ "${version}" = "7.4" || "${version}" = "8.0" || "${version}" = "8.1" ]]; then
+  if [[ "${version}" = "7.4" || "${version}" = "8.0" || "${version}" = "8.1" || "${version}" = "8.2" ]]; then
     sed -i "s/with-gd/enable-gd/g" ${version}/debian/Dockerfile
     sed -i "s/--with-png-dir=\/usr\/include\///g" ${version}/debian/Dockerfile
     sed -i "s/-dir=/=/g" ${version}/debian/Dockerfile
