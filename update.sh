8c5412f9766e2a72eceb16ccaea16df49e979862
lawxen <lawxen@outlook.com>
2024-01-12 11:00:36 +0800

    fix #10

--- update.sh
+++ update.sh
@@ -46,39 +46,39 @@ for version in ${versions[*]}; do
     sed -i "s/-dir=/=/g" ${version}/alpine/Dockerfile
   fi
 done
-for version in ${versions[*]}; do
-  if [ ! -d ${version}/debian ]; then
-    mkdir -p ${version}/debian
-  fi
-  if [[ "${version}" = "7.1" ||  "${version}" = "7.2" ]]; then
-    alpine_php_version=7
-  fi
-  if [[ "${version}" = "8.0" ]]; then
-    alpine_php_version=8
-  fi
-  if [[ "${version}" = "8.1"  ]]; then
-    alpine_php_version=81
-  fi
-  if [[ "${version}" = "8.2"  ]]; then
-    alpine_php_version=82
-  fi
-  if [[ "${version}" = "8.3"  ]]; then
-    alpine_php_version=83
-  fi
-  render Dockerfile-debian.template > $version/debian/Dockerfile
-  # https://github.com/flyve-mdm/docker-environment/issues/68
-  if [ "${version}" = "7.1" ]; then
-    sed -i "s/pecl install xdebug/pecl install xdebug-2.9.0/g" ${version}/debian/Dockerfile
-  fi
-  ## php:7.1-fpm-alpile stick on alpine 3.10.
-  if [[ "${version}" = "7.1" ||  "${version}" = "7.2" ]]; then
-    sed -i "s/libpq-dev/postgresql-libs postgresql-dev /g" ${version}/debian/Dockerfile
-  fi
-  # https://www.php.net/manual/en/image.installation.php
-  # php 7.4 gd config differenct as before.
-  if [[ "${version}" = "7.4" || "${version}" = "8.0" || "${version}" = "8.1" || "${version}" = "8.2" || "${version}" = "8.3" ]]; then
-    sed -i "s/with-gd/enable-gd/g" ${version}/debian/Dockerfile
-    sed -i "s/--with-png-dir=\/usr\/include\///g" ${version}/debian/Dockerfile
-    sed -i "s/-dir=/=/g" ${version}/debian/Dockerfile
-  fi
-done
+# for version in ${versions[*]}; do
+#   if [ ! -d ${version}/debian ]; then
+#     mkdir -p ${version}/debian
+#   fi
+#   if [[ "${version}" = "7.1" ||  "${version}" = "7.2" ]]; then
+#     alpine_php_version=7
+#   fi
+#   if [[ "${version}" = "8.0" ]]; then
+#     alpine_php_version=8
+#   fi
+#   if [[ "${version}" = "8.1"  ]]; then
+#     alpine_php_version=81
+#   fi
+#   if [[ "${version}" = "8.2"  ]]; then
+#     alpine_php_version=82
+#   fi
+#   if [[ "${version}" = "8.3"  ]]; then
+#     alpine_php_version=83
+#   fi
+#   render Dockerfile-debian.template > $version/debian/Dockerfile
+#   # https://github.com/flyve-mdm/docker-environment/issues/68
+#   if [ "${version}" = "7.1" ]; then
+#     sed -i "s/pecl install xdebug/pecl install xdebug-2.9.0/g" ${version}/debian/Dockerfile
+#   fi
+#   ## php:7.1-fpm-alpile stick on alpine 3.10.
+#   if [[ "${version}" = "7.1" ||  "${version}" = "7.2" ]]; then
+#     sed -i "s/libpq-dev/postgresql-libs postgresql-dev /g" ${version}/debian/Dockerfile
+#   fi
+#   # https://www.php.net/manual/en/image.installation.php
+#   # php 7.4 gd config differenct as before.
+#   if [[ "${version}" = "7.4" || "${version}" = "8.0" || "${version}" = "8.1" || "${version}" = "8.2" || "${version}" = "8.3" ]]; then
+#     sed -i "s/with-gd/enable-gd/g" ${version}/debian/Dockerfile
+#     sed -i "s/--with-png-dir=\/usr\/include\///g" ${version}/debian/Dockerfile
+#     sed -i "s/-dir=/=/g" ${version}/debian/Dockerfile
+#   fi
+# done
